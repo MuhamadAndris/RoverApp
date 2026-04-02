f191f9c70cfb43b1e12dfd5398b6196ffe9d461a
andris <muhamad335andris@gmail.com>
2026-04-02 23:19:31 +0700

    menambahkan tombol untuk bolak balik dari home ke report dan sebaliknya

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <>
       <h1>ini adalah halaman utama</h1>
+      <Link href="/report">ke halaman report</Link>
     </>
   )
 }
---
+++ app/report/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const ReportPage = () => {
+    return (
+        <div>
+            <h1>ini adalah halaman report</h1>
+            <Link href="/">kembali ke halaman utama</Link>
+        </div>
+    )
+}
+
+export default ReportPage;
